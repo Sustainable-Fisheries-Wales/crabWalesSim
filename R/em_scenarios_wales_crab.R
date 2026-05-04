5052571f04dc50d3cf107732a824fdc25abbdef1
dgoto2 <daisuke.goto2@gmail.com>
2026-05-04 15:45:47 +0100

    update parameter values in om & em scenarios

--- R/em_scenarios_wales_crab.R
+++ R/em_scenarios_wales_crab.R
@@ -5,9 +5,10 @@
 #' @scenario_m a flag for natural mortality misspecification (0=base, -1=underestimation, 1=overestimation)
 #' @scenario_st a flag for steepness
 #' @scenario_sl a flag for selectivity
-#' @scenario_linf a falg for asymptotic length
+#' @scenario_linf a flag for asymptotic length
 #' @scenario_ess a flag for effective sample size for size composition data
 #' @scenario_om scenario id for om
+#' 
 #' 
 em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, scenario_ess, scenario_om) {
   if (scenario_om==0) {
@@ -19,7 +20,7 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
-    if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } # max:1.22 - no difference from base
+    if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
     if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
     
     # selectivity
@@ -42,7 +43,7 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
-    if (scenario_m==-1) { m_mult1 <- 0.8; m_mult2 <- 0.8 }
+    if (scenario_m==-1) { m_mult1 <- 0.75; m_mult2 <- 0.75 }
     if (scenario_m==1) { m_mult1 <- 1.14; m_mult2 <- 1.14 }
     
     # steepness
@@ -52,13 +53,13 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
-    if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.25; sel_mult2 <- 1.25; sel_mult3 <- 1.25; sel_phase <- -1 }
+    if (scenario_sl==1) { sel_mult1 <- 0.5; sel_mult2 <- 0.5; sel_mult3 <- 0.5; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.03; linf_mult2 <- 1.03 }
+    if (scenario_linf==1) { linf_mult1 <- 0.987; linf_mult2 <- 0.987 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
@@ -70,23 +71,23 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
-    if (scenario_m==-1) { m_mult1 <- 0.83; m_mult2 <- 0.83 }
-    if (scenario_m==1) { m_mult1 <- 1.12; m_mult2 <- 1.12 }
+    if (scenario_m==-1) { m_mult1 <- 0.65; m_mult2 <- 0.65 }
+    if (scenario_m==1) { m_mult1 <- 1.07; m_mult2 <- 1.07 }
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
     if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
-    if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
+    if (scenario_st==1) { steep_mult <- 0.28 ; steep_phase <- -1 }
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
-    if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.22; sel_mult2 <- 1.22; sel_mult3 <- 1.22; sel_phase <- -1 }
+    if (scenario_sl==1) { sel_mult1 <- 0.8; sel_mult2 <- 0.8; sel_mult3 <- 0.8; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.04; linf_mult2 <- 1.04 }
+    if (scenario_linf==1) { linf_mult1 <- 0.993; linf_mult2 <- 0.993 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
@@ -99,22 +100,22 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
     if (scenario_m==-1) { m_mult1 <- 0.83; m_mult2 <- 0.83 }
-    if (scenario_m==1) { m_mult1 <- 1.12; m_mult2 <- 1.12 }
+    if (scenario_m==1) { m_mult1 <- 1.1; m_mult2 <- 1.1 }
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
     if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
-    if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
+    if (scenario_st==1) { steep_mult <- 0.25; steep_phase <- -1 }
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.3; sel_mult2 <- 1.3; sel_mult3 <- 1.3; sel_phase <- -1 }
     if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.10; linf_mult2 <- 1.10 }
+    if (scenario_linf==1) { linf_mult1 <- 0.993; linf_mult2 <- 0.993 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
@@ -127,22 +128,22 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
     if (scenario_m==-1) { m_mult1 <- 0.83; m_mult2 <- 0.83 }
-    if (scenario_m==1) { m_mult1 <- 1.12; m_mult2 <- 1.12 }
+    if (scenario_m==1) { m_mult1 <- 1.113; m_mult2 <- 1.13 }
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
-    if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
-    if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
+    if (scenario_st==-1) { steep_mult <- 1.21; steep_phase <- -1 } 
+    if (scenario_st==1) { steep_mult <- 0.77; steep_phase <- -1 }
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
-    if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.28; sel_mult2 <- 1.28; sel_mult3 <- 1.28; sel_phase <- -1 }
+    if (scenario_sl==1) { sel_mult1 <- 0.75; sel_mult2 <- 0.75; sel_mult3 <- 0.75; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.01; linf_mult2 <- 1.01 }
+    if (scenario_linf==1) { linf_mult1 <- 0.992; linf_mult2 <- 0.992 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
@@ -154,23 +155,23 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
-    if (scenario_m==-1) { m_mult1 <- 0.83; m_mult2 <- 0.83 }
+    if (scenario_m==-1) { m_mult1 <- 0.88; m_mult2 <- 0.88 }
     if (scenario_m==1) { m_mult1 <- 1.12; m_mult2 <- 1.12 }
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
-    if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
-    if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
+    if (scenario_st==-1) { steep_mult <- 1.21; steep_phase <- -1 } 
+    if (scenario_st==1) { steep_mult <- 0.4; steep_phase <- -1 }
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
-    if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.2; sel_mult2 <- 1.2; sel_mult3 <- 1.2; sel_phase <- -1 }
+    if (scenario_sl==1) { sel_mult1 <- 0.85; sel_mult2 <- 0.85; sel_mult3 <- 0.85; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.27; linf_mult2 <- 1.27 }
+    if (scenario_linf==1) { linf_mult1 <- 0.978; linf_mult2 <- 0.978 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
@@ -182,28 +183,28 @@ em_scenario <- function(scenario_m, scenario_st, scenario_sl, scenario_linf, sce
     
     # natural mortality
     if (scenario_m==0) { m_mult1 <- 1; m_mult2 <- 1 }
-    if (scenario_m==-1) { m_mult1 <- 0.83; m_mult2 <- 0.83 }
-    if (scenario_m==1) { m_mult1 <- 1.12; m_mult2 <- 1.12 }
+    if (scenario_m==-1) { m_mult1 <- 0.92; m_mult2 <- 0.92 }
+    if (scenario_m==1) { m_mult1 <- 1.09; m_mult2 <- 1.09 }
     
     # steepness
     if (scenario_st==0) { steep_mult <- 1; steep_phase <- 1 }
-    if (scenario_st==-1) { steep_mult <- 1.22; steep_phase <- -1 } 
-    if (scenario_st==1) { steep_mult <- 0.3; steep_phase <- -1 }
+    if (scenario_st==-1) { steep_mult <- 1.208; steep_phase <- -1 }
+    if (scenario_st==1) { steep_mult <- 0.27; steep_phase <- -1 }
     
     # selectivity
     if (scenario_sl==0) { sel_mult1 <- 1; sel_mult2 <- 1; sel_mult3 <- 1; sel_phase <- 1 }
-    if (scenario_sl==-1) { sel_mult1 <- 1.42; sel_mult2 <- 1.42; sel_mult3 <- 1.42; sel_phase <- -1 }
-    if (scenario_sl==1) { sel_mult1 <- 0.72; sel_mult2 <- 0.72; sel_mult3 <- 0.72; sel_phase <- -1 }
+    if (scenario_sl==-1) { sel_mult1 <- 1.32; sel_mult2 <- 1.32; sel_mult3 <- 1.32; sel_phase <- -1 }
+    if (scenario_sl==1) { sel_mult1 <- 0.70; sel_mult2 <- 0.70; sel_mult3 <- 0.70; sel_phase <- -1 }
     
     # asymptotic length
     if (scenario_linf==0) { linf_mult1 <- 1; linf_mult2 <- 1 }
-    if (scenario_linf==-1) { linf_mult1 <- 1.09; linf_mult2 <- 1.09 }
-    if (scenario_linf==1) { linf_mult1 <- 0.9813; linf_mult2 <- 0.9813 }
+    if (scenario_linf==-1) { linf_mult1 <- 1.17; linf_mult2 <- 1.17 } 
+    if (scenario_linf==1) { linf_mult1 <- 0.98; linf_mult2 <- 0.98 }
     
     # effective sample size
     if (scenario_ess==0) { ess <- 100 }
     if (scenario_ess==-1) { ess <- 20 }
     if (scenario_ess==1) { ess <- 200 }
   }
-  return(c(m_mult1, m_mult2, steep_mult, steep_phase, sel_mult1, sel_mult2, sel_mult3, sel_phase, linf_mult1, linf_mult2, ess))
+  return(c(m_mult1, m_mult2, steep_mult, steep_phase, sel_mult1, sel_mult2, sel_mult3, sel_phase, linf_mult1, linf_mult2, ess ))
 }
--- R/om_scenarios_wales_crab.R
+++ R/om_scenarios_wales_crab.R
@@ -1,5 +1,5 @@
 #' identify OM scenario to set multiplier values for fishing mortality and recruitment deviation (F_multi, sigmaR_dev)
-#' @scenario_om scenario id for OM - high and low Fs and high and low sigmaR
+#' @scenario_om scenario id for OM
 #' 
 #' 
 om_scenario <- function(scenario_om = scenario_om) {
@@ -7,10 +7,10 @@ om_scenario <- function(scenario_om = scenario_om) {
   if (scenario_om==0) { F_multi <- 1; sigmaR_dev <- 1 }
   if (scenario_om==1) { F_multi <- 0.5; sigmaR_dev <- 0.25 }
   if (scenario_om==2) { F_multi <- 0.5; sigmaR_dev <- 0.95 }
-  if (scenario_om==3) { F_multi <- 0.5; sigmaR_dev <- 2.125 }
+  if (scenario_om==3) { F_multi <- 0.5; sigmaR_dev <- 1.276 }
   if (scenario_om==4) { F_multi <- 1.5; sigmaR_dev <- 0.25 }
   if (scenario_om==5) { F_multi <- 1.5; sigmaR_dev <- 0.95 }
-  if (scenario_om==6) { F_multi <- 1.5; sigmaR_dev <- 2.125 }
+  if (scenario_om==6) { F_multi <- 1.5; sigmaR_dev <- 1.276 }
   
   return(c(F_multi, sigmaR_dev))
 }
